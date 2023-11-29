b76c6e0ac92665d42399f071557e3d3896f1c9ae
shipherman <aleksandr.tyagni@gmail.com>
2023-11-29 16:18:39 +0300

    Fix routes

--- internal/transport/routes/routes.go
+++ internal/transport/routes/routes.go
@@ -4,15 +4,14 @@ import (
 	"github.com/shipherman/speech-to-text/internal/handlers"
 
 	chi "github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 var Router *chi.Mux
 
 func init() {
-	// Router = chi.NewRouter()
+	Router = chi.NewRouter()
 
-	Router.Use(middleware.Logger)
+	// Router.Use(middleware.Logger)
 	Router.Post("/stt", handlers.HandleSTT)
 	Router.Post("/api/user/register", handlers.HandleRegister)
 	Router.Post("/api/user/login", handlers.HandleLogin)
