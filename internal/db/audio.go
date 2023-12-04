9a4cb24305432b37d5ad70b6c7901403859dba43
shipherman <aleksandr.tyagni@gmail.com>
2023-12-05 01:22:47 +0300

    Add audio SAve func

---
+++ internal/db/audio.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"github.com/shipherman/speech-to-text/gen/ent"
+)
+
+func Save(a ent.Audio, u ent.User) error {
+	// h := sha256.New()
+	// _, err := hashAudio.Write(a.)
+	// audioHash := h.Sum(nil)
+
+	return nil
+}
