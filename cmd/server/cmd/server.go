7e0b3883a83446134a5ccf4cec413f3de02e9260
shipherman <aleksandr.tyagni@gmail.com>
2023-12-07 18:52:03 +0300

    Rename server file

--- cmd/server/cmd/server.go
+++ cmd/server/cmd/server.go
@@ -2,25 +2,26 @@ package cmd
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 
 	sttservice "github.com/shipherman/speech-to-text/gen/stt/service/v1"
 	"github.com/shipherman/speech-to-text/internal/clients"
+	"github.com/shipherman/speech-to-text/pkg/audioconverter"
+	"github.com/shipherman/speech-to-text/pkg/fsstore"
 )
 
 type TranscribeServer struct {
 	sttservice.UnimplementedSttServiceServer
 }
 
+var LocStore *fsstore.FSStore
+
 func (t *TranscribeServer) TranscribeAudio(
 	audio *sttservice.Audio,
 	stream sttservice.SttService_TranscribeAudioServer,
 ) error {
-	text, err := clients.ReqSTT(audio.Audio)
-	if err != nil {
-		return err
-	}
-
 	// TODO
 	// Implement pipline for processing audio
 	// 1. Check file, if it's a valid wav file
@@ -34,8 +35,38 @@ func (t *TranscribeServer) TranscribeAudio(
 	// Additional
 	//	convert *.ogg to *.wav [for telegram bot]
 
-	response := &sttservice.Status{Status: sttservice.EnumStatus_STATUS_ACCEPTED}
+	var response *sttservice.Status
+
+	// Check wave header and if its valid send status ACCEPTED
+	_, err := audioconverter.CheckWAVHeader(audio.Audio)
+	if err != nil {
+		return err
+	}
+	response = &sttservice.Status{Status: sttservice.EnumStatus_STATUS_ACCEPTED}
 	stream.Send(response)
+
+	// Calculate hash sum
+	h := md5.New()
+	_, err = h.Write(audio.Audio)
+	if err != nil {
+		return err
+	}
+	// Use hex string as filename
+	audioFileHashSum := hex.EncodeToString(h.Sum(nil))
+
+	// Save data to DB
+
+	// Save audio to store
+	LocStore = fsstore.NewFSStore()
+	LocStore.Configure("/tmp/stt/store")
+	LocStore.Save(string(audioFileHashSum), audio.Audio)
+
+	// Call remote STT neural network service
+	text, err := clients.ReqSTT(audio.Audio)
+	if err != nil {
+		return err
+	}
+
 	response = &sttservice.Status{Status: sttservice.EnumStatus_STATUS_ORDERED}
 	stream.Send(response)
 	response = &sttservice.Status{Status: sttservice.EnumStatus_STATUS_DONE}
