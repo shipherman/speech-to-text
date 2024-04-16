394afbb35f79535a699236df321c964c476a6d63
shipherman <aleksandr.tyagni@gmail.com>
2024-04-16 22:19:53 +0300

    Change doc files

---
+++ cmd/server/doc.go
@@ -0,0 +1,25 @@
+// Package server implements Speech To Text server that trancribes your audio to a text
+/*
+
+
+# STT Docker image
+
+
+# STT repo
+
+ git clone git@github.com:shipherman/stt.git
+
+
+# Models for STT
+
+# Enlish speech model
+
+https://storage.yandexcloud.net/stt-models/english-model.tflite
+https://storage.yandexcloud.net/stt-models/english-vocabulary.scorer
+
+# Russian speech model
+
+https://storage.yandexcloud.net/stt-models/russian-model.tflite
+https://storage.yandexcloud.net/stt-models/russian-vocabulary.scorer
+*/
+package main
